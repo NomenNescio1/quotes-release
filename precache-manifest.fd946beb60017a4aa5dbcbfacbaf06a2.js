8a26e8140bec93c8084a6aa60db09244de6b94db
NomenNescio <u1201416@unimilitar.edu.co>
2020-01-20 23:48:43 -0500

    Updates

---
+++ precache-manifest.fd946beb60017a4aa5dbcbfacbaf06a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a271791045ea5a33efb0038b8d4ec26",
+    "url": "/quotes-release/index.html"
+  },
+  {
+    "revision": "dcc25cf5895bce8bb394",
+    "url": "/quotes-release/static/css/main.3296c351.chunk.css"
+  },
+  {
+    "revision": "e51515867d01236a8c98",
+    "url": "/quotes-release/static/js/2.381ba4ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quotes-release/static/js/2.381ba4ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcc25cf5895bce8bb394",
+    "url": "/quotes-release/static/js/main.8f9d34b7.chunk.js"
+  },
+  {
+    "revision": "93419ae96271cbd51fdb",
+    "url": "/quotes-release/static/js/runtime-main.f57bed6f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simpsons-quotes/precache-manifest.079b458bc248341d212356d1c7b6956f.js"
+  "/quotes-release/precache-manifest.fd946beb60017a4aa5dbcbfacbaf06a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simpsons-quotes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quotes-release/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
